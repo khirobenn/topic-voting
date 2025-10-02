d5d0d9da04d354ebc243a24317d20e6dbc1dfd8d
khirobenn <khireddine.benmeziane@edu.univ-paris13.fr>
2025-10-03 01:46:40 +0200

    adding all cases

--- vote.py
+++ vote.py
@@ -3,7 +3,8 @@ import pulp as p
 
 nb_etudiants = 26 # Le nombre d'étudiants dans la classe
 nb_sujets = 5 # Le nombre de sujets proposés
-
+my_solutions = []
+my_maximums = []
 produit = nb_etudiants*nb_sujets
 
 problem = p.LpProblem('objective_function', p.LpMaximize)
@@ -30,34 +31,48 @@ for i in range(0, nb_etudiants):
 
 # L'ajout des contraintes sur les colonnes
 # le k faut le faire varier après pour obtenir tout les cas possible
-k = 0
-for i in range(0,  nb_sujets):
-    contrainst = 0
-    if i == k:
-        contrainst = -6
-    else:
-        contrainst = -5
+for k in range(0, 5):
+    for i in range(0,  nb_sujets):
+        contrainst = 0
+        if i == k:
+            contrainst = -6
+        else:
+            contrainst = -5
 
-    for j in range(0, nb_etudiants):
-        contrainst = contrainst + Y[j*5 + i]
-    problem += contrainst == 0
+        for j in range(0, nb_etudiants):
+            contrainst = contrainst + Y[j*5 + i]
+        problem += contrainst == 0
 
-# Maintenant on résoud le système
-status = problem.solve()
-print(p.LpStatus[status])
+    # Maintenant on résoud le système
+    status = problem.solve()
+
+    # On stocke le maximum pour chaque k
+    my_maximums.append(problem.objective.value())
 
 
-# ici, on récupère la solution
-solution = []
+    # ici, on récupère la solution
+    solution = []
 
-for i in range(0, nb_etudiants):
-    tmp = []
-    for j in range(0, nb_sujets):
-        tmp.append(p.value(Y[i*5 + j]))
-    solution.append(tmp)
+    # On enlève les contraintes mise sur les colonnes
+    # pour les mettre à jour à l'incrémentation du k
+    for i in range(0, nb_etudiants):
+        tmp = []
+        for j in range(0, nb_sujets):
+            tmp.append(p.value(Y[i*5 + j]))
+        solution.append(tmp)
+    # on affiche la solution et on vérifie si les contraintes sont respectées
+    solution = np.array(solution)
 
-# on affiche la solution et on vérifie si les contraintes sont respectées
-solution = np.array(solution)
-print(solution)
-print(solution.sum(axis=0))
-print(solution.sum(axis=1))
+    # On stocke la solution pour le k courant
+    my_solutions.append(solution)
+
+    for i in range(0, 5):
+        problem.constraints.popitem()
+
+
+print(my_solutions)
+print(my_maximums)
+
+# Notre solution est celle qui donne la plus grande valeurs dans my_maximum
+my_sol = my_solutions[np.argmax(my_maximums)]
+print("Solution: ", my_sol)
