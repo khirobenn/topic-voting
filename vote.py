d92f769175bd2f502837ed6e105b0ef80af1b032
khirobenn <khireddine.benmeziane@edu.univ-paris13.fr>
2025-10-06 20:03:28 +0200

    some chenges

--- vote.py
+++ vote.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import pulp as p
 
-nb_etudiants = 26 # Le nombre d'étudiants dans la classe
+nb_etudiants = 25 # Le nombre d'étudiants dans la classe
 nb_sujets = 5 # Le nombre de sujets proposés
 my_solutions = []
 my_maximums = []
@@ -52,52 +52,29 @@ for i in range(0, nb_etudiants):
 
 # L'ajout des contraintes sur les colonnes
 # le k faut le faire varier après pour obtenir tout les cas possible
-for k in range(0, 5):
-    for i in range(0,  nb_sujets):
-        contrainst = 0
-        if i == k:
-            contrainst = -6
-        else:
-            contrainst = -5
+for i in range(0,  nb_sujets):
+    contrainst = -5
+    for j in range(0, nb_etudiants):
+        contrainst = contrainst + Y[j*5 + i]
+    problem += contrainst == 0
 
-        for j in range(0, nb_etudiants):
-            contrainst = contrainst + Y[j*5 + i]
-        problem += contrainst == 0
+# Maintenant on résoud le système
+status = problem.solve()
 
-    # Maintenant on résoud le système
-    status = problem.solve()
-
-    # ici, on récupère la solution dans une matrice de (nb_etudiants x nb_sujets)
-    solution = []
-
-    
-    for i in range(0, nb_etudiants):
-        tmp = []
-        for j in range(0, nb_sujets):
-            tmp.append(p.value(Y[i*5 + j]))
-        solution.append(tmp)
-
-    solution = np.array(solution)
-
-    # On stocke la solution pour le k courant
-    my_solutions.append(solution)
-    # On stocke le résultat de la fonction objective (son maximum) pour le k courant
-    my_maximums.append(problem.objective.value())
-
-    # On enlève les contraintes mise sur les colonnes
-    # pour les mettre à jour à l'incrémentation du k
-    for i in range(0, 5):
-        problem.constraints.popitem()
+# ici, on récupère la solution dans une matrice de (nb_etudiants x nb_sujets)
+solution = []
 
 
-print(my_solutions)
-print(my_maximums)
+for i in range(0, nb_etudiants):
+    tmp = []
+    for j in range(0, nb_sujets):
+        tmp.append(p.value(Y[i*5 + j]))
+    solution.append(tmp)
 
-# Notre solution est celle qui donne la plus grande valeurs dans my_maximum
-my_sol = my_solutions[np.argmax(my_maximums)]
+solution = np.array(solution)
 
-groups = np.argmax(my_sol, axis=1)+1
-print("Solution: ", my_sol)
+groups = np.argmax(solution, axis=1)+1
+print("Solution: ", solution)
 
 df["Groupe"] = groups
 
